f3a74b6804854a408a67955fec5b99b15a878685
Igor Khotin <khotin@gmx.com>
2020-10-30 22:29:20 +0200

    [*] comments for mover.mod

--- mover.mod/lab.js
+++ mover.mod/lab.js
@@ -1,12 +1,13 @@
 // coordinates and radius
-let   x = rx(.5)
-const y = ry(.5)
-const r = ry(.05)
+let   x = rx(.5)  // center horizontally
+const y = ry(.5)  // center vertically
+const r = ry(.05) // 5% of the screen height
 
-const SPEED = rx(.33)
+const SPEED = rx(.25) // 25% of the screen width per second
 
 function evo(dt) {
     // move the circle horizontally
+    // warp the coordinate, so it always stays on the screen
     x = warp(x + SPEED*dt, 0, rx(1))
 }
 
